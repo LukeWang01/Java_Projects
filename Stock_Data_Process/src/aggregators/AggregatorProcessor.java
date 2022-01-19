929b7b05904b6889a368fce426a1cacf223a7541
Wang, Luke <zwang3208@gatech.edu>
2022-01-18 23:23:03 -0500

    01.18.22
    revise aggregator

--- Stock_Data_Process/src/aggregators/AggregatorProcessor.java
+++ Stock_Data_Process/src/aggregators/AggregatorProcessor.java
@@ -1,11 +1,35 @@
 package aggregators;
 
-public class AggregatorProcessor {
 
+import fileprocessors.StockFileReader;
 
-    public double runAggregator(int i) {
-        double requestValue = 0;
-        
+import java.io.IOException;
+import java.util.List;
+
+public class AggregatorProcessor <T extends Aggregator> {
+    // define the class
+    T aggregator;
+    String file;
+
+    public AggregatorProcessor(T aggregator, String file){
+        super();
+        this.aggregator = aggregator;
+        this.file = file;
+    }
+
+    public double runAggregator(int i) throws IOException {
+        double requestValue;
+        StockFileReader fileReader = new StockFileReader(file);
+        List<String> lines =  fileReader.readFileData();
+        i--;
+        for(String line : lines) {
+            String [] number = line.split(","); // save the data from the line to an array.
+            Double value = Double.parseDouble(number[i]);
+            aggregator.add(value);
+        }
+
+        requestValue = aggregator.calculate();
+
 
         return requestValue;
     }
